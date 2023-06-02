fd3bd83548d87bfff2cd0fe398c8fee8c69f3682
Rufat Aliyev <rufat845@gmail.com>
2023-06-02 17:27:30 +0400

    services

--- src/components/Service/Service.jsx
+++ src/components/Service/Service.jsx
@@ -1,7 +1,31 @@
-import React from "react";
-
-const Service = () => {
-  return <div>Service</div>;
+import Button from "../../UI/Button";
+const Service = ({ data }) => {
+  return (
+    <div className="flex flex-row group even:flex-row-reverse mt-[72px] gap-5 justify-between w-full">
+      <div className="w-5/12 text-left flex flex-col justify-center items-start align-center gap-7 max-w-[485px]">
+        <h1 className="text-2xl text-purple-400 font-primary">{data.name}</h1>
+        <p className="text-base text-green-500 font-montserrat leading-6">
+          {data.description}
+        </p>
+        <Button
+          styles={
+            "bg-green-400 py-[14px] px-[56px] hover:bg-green-400/60 inline-block  hover:text-black font-montserrat transition-all text-[15px] text-white"
+          }
+        >
+          Ətraflı
+        </Button>
+      </div>
+      <div className="w-7/12">
+        <div className="w-full bg-green-400 h-full">
+          <img
+            className="object-cover w-full group-even:translate-x-1.5 group-even:translate-y-1.5 group-odd:-translate-x-1.5 group-odd:-translate-y-1.5 h-full"
+            src={data.image}
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Service;
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,13 +1,35 @@
+import { useEffect, useState } from "react";
 import leaf2 from "../../assets/img/image-2.png";
+import Service from "../Service/Service";
+import axios from "axios";
 const Services = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        "https://0b40386c-03d9-4e62-91e5-d60c317525b7.mock.pstmn.io/services"
+      );
+      setData(response.data);
+    } catch (error) {
+      console.log("Error fetching data:", error);
+    }
+  };
   return (
-    <div className="text-center flex-col items-center relative container flex mt-24 mb-32 z-50">
+    <div className="text-center flex-col items-center relative container max-w-[1172px] flex mt-24 mb-32 z-50">
       <h1 className="text-4xl text-purple-400 font-primary">Xidmətlərimiz</h1>
       <p className="font-montserrat text-green-500 mt-4 text-base max-w-[420px]">
         Lorem Ipsum is simply dummy text of the printing and typesetting
         industry
       </p>
-      <img src={leaf2} className="absolute right-2/4 -bottom-2/4 z-10" alt="" />
+      {/* <img src={leaf2} className="absolute right-2/4 -bottom-2/4 z-10" alt="" /> */}
+      {data.map((item, idx) => (
+        <Service key={item.id} data={item} />
+      ))}
     </div>
   );
 };
