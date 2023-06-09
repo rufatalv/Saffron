eb756b7f0089af541ca01797476b0dd8b0fd78dc
Rufat Aliyev <rufat845@gmail.com>
2023-06-09 15:09:50 +0400

    final

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,7 +5,9 @@ const Hero = () => {
       className="container relative px-5 xl:px-0 z-20 mt-24 lg:mt-32"
       id="hero"
     >
-      <img src={heroBG} />
+      <div className="h-72 w-auto md:h-auto">
+        <img className="object-cover h-full" src={heroBG} />
+      </div>
       <div className="max-w-[427px] flex gap-8 md:gap-3 flex-col absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
         <h1 className="text-center font-primary text-purple-500 text-xl sm:text-3xl md:text-5xl">
           Saffron Aqro
